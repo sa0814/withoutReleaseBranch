96bf51ca35a877c1415a697af8be0150a4285dd5
sa0814 <92997651+sa0814@users.noreply.github.com>
2023-06-28 10:33:37 -0400

    Add files via upload

---
+++ OWO_Folder/README.md
@@ -0,0 +1,2 @@
+# test_dc_simulink_repo
+A test repository for trying out view generation of Simulink models in design cloud
---
+++ OWO_Folder/runTheModel.m
@@ -0,0 +1,7 @@
+% comment to force rebuild of views
+model = 'mf14_mdl.mdl';
+simout = sim(model) %#ok<NOPTS> 
+
+% Comment we can add more text to with linux pipe to script lots of commits:
+% xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+% dummy test for bash 
